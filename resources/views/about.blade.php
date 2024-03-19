dede76631ba7daf176de9e78b27674cf56135844
raihan6661 <raihanghifariali07@gmail.com>
2024-03-19 21:49:19 +0700

    second commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,5 +4,5 @@
   <h1>Halaman About</h1>
   <h3>{{ $name }}</h3>
   <p>{{ $email }}</p>
-  <img src="assets/img/{{ $img }}" alt="$name" width=100>
+  <img src="assets/img/{{ $img }}" alt="{{ $name }}" width=100 class="img-thumnail rounded-circle">
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/about', function () {
         "title" => "About",
         "name" => "raihan GAH",
         "email" => "raihangah@gmail.com",
-        "img" => "logos.png"
+        "img" => "logo.png"
     ]);
 });
 
@@ -25,6 +25,7 @@ Route::get('/blog', function () {
     $blog_spot = [
         [
             "title" => "judul pertama",
+            "slug" => "judul_Post_Pertama",
             "author" => "Raihan",
             "body" => "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloremque facere illo animi sed. 
             Suscipit ab dolores sequi ex sint. Voluptate esse saepe quae sint tempora quis praesentium veritatis ab 
@@ -35,6 +36,39 @@ Route::get('/blog', function () {
         ],
         [
             "title" => "judul kedua",
+            "slug" => "judul_Post_Kedua",
+            "author" => "panji",
+            "body" => "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloremque facere illo animi sed. 
+            Suscipit ab dolores sequi ex sint. Voluptate esse saepe quae sint tempora quis praesentium veritatis ab 
+            porro repellendus expedita consequuntur aspernatur et quibusdam, earum soluta, veniam debitis ratione eius 
+            obcaecati impedit facilis, rerum cum repudiandae? Vel ducimus expedita voluptate illum incidunt deserunt. 
+            Cumque, quisquam id cum in optio repellendus maiores perferendis non velit vel numquam eum, porro est molestiae 
+            quasi voluptatem ipsa? Optio, perferendis. Eos, esse?"
+        ]
+    ];
+    return view('Blog',
+    [
+        "title" => "Blog",
+        "posts" => $blog_spot
+    ]);
+});
+
+Route::get('blog/{slug}', function($slug){
+    $blog_spot = [
+        [
+            "title" => "judul pertama",
+            "slug" => "judul_Post_Pertama",
+            "author" => "Raihan",
+            "body" => "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloremque facere illo animi sed. 
+            Suscipit ab dolores sequi ex sint. Voluptate esse saepe quae sint tempora quis praesentium veritatis ab 
+            porro repellendus expedita consequuntur aspernatur et quibusdam, earum soluta, veniam debitis ratione eius 
+            obcaecati impedit facilis, rerum cum repudiandae? Vel ducimus expedita voluptate illum incidunt deserunt. 
+            Cumque, quisquam id cum in optio repellendus maiores perferendis non velit vel numquam eum, porro est molestiae 
+            quasi voluptatem ipsa? Optio, perferendis. Eos, esse?"
+        ],
+        [
+            "title" => "judul kedua",
+            "slug" => "judul_Post_Kedua",
             "author" => "panji",
             "body" => "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloremque facere illo animi sed. 
             Suscipit ab dolores sequi ex sint. Voluptate esse saepe quae sint tempora quis praesentium veritatis ab 
@@ -45,10 +79,14 @@ Route::get('/blog', function () {
         ]
     ];
 
-    return view('Blog',
-    [
-        "title" => "Blog",
-        "posts" => $blog_spot
+    $new_post = [];
+    foreach($blog_spot as $post){
+        if($post["slug"] === $slug){
+            $new_post = $post;
+        }
+    }
+    return view('post',[
+        "title" => "single post",
+        "post" => $new_post
     ]);
 });
-
