05e2e1c03dff85c479ee87f5c39060e834456263
raihan6661 <raihanghifariali07@gmail.com>
2024-03-20 16:36:51 +0700

    theres commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,38 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Post extends Model
+class Post
 {
-    use HasFactory;
+    private static $blog_posts = [
+        [
+            "title" => "judul pertama",
+            "slug" => "judul_Post_Pertama",
+            "author" => "Raihan",
+            "body" => "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloremque facere illo animi sed. 
+            Suscipit ab dolores sequi ex sint. Voluptate esse saepe quae sint tempora quis praesentium veritatis ab 
+            porro repellendus expedita consequuntur aspernatur et quibusdam, earum soluta, veniam debitis ratione eius 
+            obcaecati impedit facilis, rerum cum repudiandae? Vel ducimus expedita voluptate illum incidunt deserunt. 
+            Cumque, quisquam id cum in optio repellendus maiores perferendis non velit vel numquam eum, porro est molestiae 
+            quasi voluptatem ipsa? Optio, perferendis. Eos, esse?"
+        ],
+        [
+            "title" => "judul kedua",
+            "slug" => "judul_Post_Kedua",
+            "author" => "panji",
+            "body" => "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloremque facere illo animi sed. 
+            Suscipit ab dolores sequi ex sint. Voluptate esse saepe quae sint tempora quis praesentium veritatis ab 
+            porro repellendus expedita consequuntur aspernatur et quibusdam, earum soluta, veniam debitis ratione eius 
+            obcaecati impedit facilis, rerum cum repudiandae? Vel ducimus expedita voluptate illum incidunt deserunt. 
+            Cumque, quisquam id cum in optio repellendus maiores perferendis non velit vel numquam eum, porro est molestiae 
+            quasi voluptatem ipsa? Optio, perferendis. Eos, esse?"
+        ]
+    ];
+    public static function all(){
+        return collect(self::$blog_posts);
+    }
+
+    public static function find($slug){
+        $posts = static::all();
+        return $posts->firstWhere('slug', $slug);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Post;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('home',[
@@ -21,72 +22,6 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_spot = [
-        [
-            "title" => "judul pertama",
-            "slug" => "judul_Post_Pertama",
-            "author" => "Raihan",
-            "body" => "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloremque facere illo animi sed. 
-            Suscipit ab dolores sequi ex sint. Voluptate esse saepe quae sint tempora quis praesentium veritatis ab 
-            porro repellendus expedita consequuntur aspernatur et quibusdam, earum soluta, veniam debitis ratione eius 
-            obcaecati impedit facilis, rerum cum repudiandae? Vel ducimus expedita voluptate illum incidunt deserunt. 
-            Cumque, quisquam id cum in optio repellendus maiores perferendis non velit vel numquam eum, porro est molestiae 
-            quasi voluptatem ipsa? Optio, perferendis. Eos, esse?"
-        ],
-        [
-            "title" => "judul kedua",
-            "slug" => "judul_Post_Kedua",
-            "author" => "panji",
-            "body" => "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloremque facere illo animi sed. 
-            Suscipit ab dolores sequi ex sint. Voluptate esse saepe quae sint tempora quis praesentium veritatis ab 
-            porro repellendus expedita consequuntur aspernatur et quibusdam, earum soluta, veniam debitis ratione eius 
-            obcaecati impedit facilis, rerum cum repudiandae? Vel ducimus expedita voluptate illum incidunt deserunt. 
-            Cumque, quisquam id cum in optio repellendus maiores perferendis non velit vel numquam eum, porro est molestiae 
-            quasi voluptatem ipsa? Optio, perferendis. Eos, esse?"
-        ]
-    ];
-    return view('Blog',
-    [
-        "title" => "Blog",
-        "posts" => $blog_spot
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
-Route::get('blog/{slug}', function($slug){
-    $blog_spot = [
-        [
-            "title" => "judul pertama",
-            "slug" => "judul_Post_Pertama",
-            "author" => "Raihan",
-            "body" => "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloremque facere illo animi sed. 
-            Suscipit ab dolores sequi ex sint. Voluptate esse saepe quae sint tempora quis praesentium veritatis ab 
-            porro repellendus expedita consequuntur aspernatur et quibusdam, earum soluta, veniam debitis ratione eius 
-            obcaecati impedit facilis, rerum cum repudiandae? Vel ducimus expedita voluptate illum incidunt deserunt. 
-            Cumque, quisquam id cum in optio repellendus maiores perferendis non velit vel numquam eum, porro est molestiae 
-            quasi voluptatem ipsa? Optio, perferendis. Eos, esse?"
-        ],
-        [
-            "title" => "judul kedua",
-            "slug" => "judul_Post_Kedua",
-            "author" => "panji",
-            "body" => "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloremque facere illo animi sed. 
-            Suscipit ab dolores sequi ex sint. Voluptate esse saepe quae sint tempora quis praesentium veritatis ab 
-            porro repellendus expedita consequuntur aspernatur et quibusdam, earum soluta, veniam debitis ratione eius 
-            obcaecati impedit facilis, rerum cum repudiandae? Vel ducimus expedita voluptate illum incidunt deserunt. 
-            Cumque, quisquam id cum in optio repellendus maiores perferendis non velit vel numquam eum, porro est molestiae 
-            quasi voluptatem ipsa? Optio, perferendis. Eos, esse?"
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_spot as $post){
-        if($post["slug"] === $slug){
-            $new_post = $post;
-        }
-    }
-    return view('post',[
-        "title" => "single post",
-        "post" => $new_post
-    ]);
-});
+Route::get('blog/{slug}', [PostController::class, 'show']);
